c555ef2d350131dd39b0b7ebba784a88c6f3a168
Shivam Thakur <shivamth060@gmail.com>
2024-07-08 11:09:51 +0530

    Basic Setup

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import React from "react";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "800"],
+});
+
+export const metadata: Metadata = {
+  title: "Veebot",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
